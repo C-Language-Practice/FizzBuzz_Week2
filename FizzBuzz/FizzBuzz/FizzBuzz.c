561eaf8941a9d36b651094018cbaf08ec9363291
Krisz Kaszas <kriszrokakaszas@gmail.com>
2020-12-13 14:45:54 +0100

    done

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	for (int i = 1; i <= 20; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf_s("fizz");
+		}
+		if (i % 5 == 0)
+		{
+			printf_s("buzz");
+		}
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf_s("%d", i);
+		}
+		printf_s("\n");
+	}
+}
